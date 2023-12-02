4e341773036dac6f2bd2606cadc152191a1730d0
DeAngelo Jones <deangelojones0708@gmail.com>
2023-12-02 17:57:01 -0600

    added starter files and modules

---
+++ assets/server.js
@@ -0,0 +1,9 @@
+// npm init -y
+// npm i inquirer@8.2.4
+// npm i mysql2
+const mysql = require('mysql2') // Interacts with the MySQL database
+const inquirer = require('inquirer') // Used for prompt questions
+
+
+
+
