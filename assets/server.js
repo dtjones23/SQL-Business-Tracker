dccc1064262fd718b6bc4d5c3025a62cd0456c09
DeAngelo Jones <deangelojones0708@gmail.com>
2023-12-02 18:05:16 -0600

    added connection to MySQL

--- assets/server.js
+++ assets/server.js
@@ -4,6 +4,19 @@
 const mysql = require('mysql2') // Interacts with the MySQL database
 const inquirer = require('inquirer') // Used for prompt questions
 
+// Centralize database configuration
+const dbConfig = {
+    host: 'localhost',
+    // MySQL username
+    user: 'root',
+    // MySQL password here
+    password: '',
+    database: 'employee_db'
+  };
+
+// Create a mySQL connection using the configuration
+const db = mysql.createConnection(dbConfig);
+
 
 
 
